7645afd1c238ffb5c3f09f5726bd2dace506a8a8
regi <cabael.regine@gmail.com>
2022-03-29 13:07:21 +0200

    prezzoConSconti

--- main.js
+++ main.js
@@ -3,38 +3,41 @@ const sviluppoBackEnd = 20.5;
 const sviluppoFrontEnd = 15.3;
 const projectAnalist = 33.6;
 
+//codici sconti
 let listaSconti = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+
+//dichiarazioni variabili
+let ore, lavoro, prezzo, prezzofin, sconto, valoreSconto;
 
 //calcolo prezzo lavoro
 function calcoloPrezzo() {
     event.preventDefault();
 
-    let ore = document.getElementById("ore").value; //ore richieste
+    ore = document.getElementById("ore").value; //ore richieste
     ore = parseInt(ore);
     console.log(typeof ore);
     console.log(ore);
 
-    let lavoro = document.getElementById("lavoro").value; //tipo di lavoro
+    lavoro = document.getElementById("lavoro").value; //tipo di lavoro
     console.log(typeof lavoro);
     console.log(lavoro);
 
-    let sconto = document.getElementById("sconto").value; //codice sconto
-    console.log(typeof sconto);
-
-    let prezzofin;
+    valoreSconto = sales();
 
     switch (lavoro) {
         case "1": //sviluppo BackEnd
-            prezzofin = ore * sviluppoBackEnd;
-            
+            prezzo = (ore * sviluppoBackEnd);
+            prezzofin = prezzo - (prezzo * valoreSconto);
             break;
         case "2": //sviluppo FrontEnd
-            prezzofin = ore * sviluppoFrontEnd;
+            prezzo = ore * sviluppoFrontEnd;
+            prezzofin = prezzo - (prezzo * valoreSconto);
 
             break;
         case "3":
-            prezzofin = ore * projectAnalist;
-
+            prezzo = ore * projectAnalist;
+            prezzofin = prezzo - (prezzo * valoreSconto);
+            
             break;
     }
 
@@ -42,4 +45,26 @@ function calcoloPrezzo() {
     console.log(prezzofin);
 
     document.getElementById("risultato").innerHTML = "Il prezzo finale è di : " + prezzofin + "\u20AC";
+}
+
+
+//funzione per capire se ha diritto ad uno sconto
+function sales() { 
+
+    sconto = document.getElementById("sconto").value; //codice sconto
+    sconto = sconto.toUpperCase();
+    console.log(typeof sconto);
+    console.log(sconto);
+
+    valoreSconto = 0;
+
+    for (let i = 0; i < listaSconti; i++){
+        if (sconto == listaSconti[i]) {
+            //ha diritto ad uno sconto
+            valoreSconto = 0.25;
+            break;
+        }
+    } //se non trova il codice, esce dal for con valoreSconto = 0
+
+    return valoreSconto;
 }
